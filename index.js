835c11a93960a6cfe42cc1deadf0de47d6c370bd
Abhay Singh <96994618+AbhayAnilark@users.noreply.github.com>
2024-04-22 15:59:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function rolldice(){
+    const x=document.getElementById("sel1").value;
+    const dice=[]
+    const image=[]
+    for(let i=0;i<x;i++){
+        const y=  Math.floor(Math.random()*6) +1;
+        dice.push(y); 
+        image.push(`<img src="dices/${y}.png">`);
+    
+    }
+    console.log(dice);
+    console.log(image);
+    
+    document.getElementById("num").textContent = `dice: ${ dice.join(',') }`;
+    document.getElementById("images").innerHTML = image.join('');
+
+}
